dd5232fb31fceab1d15d9f617f6e5409c2ea32ce
Perehinets Maksym <supercomf128@gmail.com>
2024-03-11 22:33:55 +0200

    test

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "resource_groupe_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "storage_account_name" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
